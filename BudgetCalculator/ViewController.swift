01ff1749d992853dc4359b9da9ad65c9adb7ce2e
fiston madimba <fistonmadimba@fistons-macbook-air.local>
2018-10-10 21:40:07 -0400

    Initial Commit

---
+++ BudgetCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BudgetCalculator
+//
+//  Created by fiston madimba on 10/10/18.
+//  Copyright © 2018 fiston madimba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
