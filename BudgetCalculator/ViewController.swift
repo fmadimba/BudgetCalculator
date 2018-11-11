a6df46e3757358a933d8fa189569ec18d05320c9
fiston madimba <fistonmadimba@fistons-macbook-air.local>
2018-11-10 21:17:23 -0500

    trying to pull.

--- BudgetCalculator/ViewController.swift
+++ BudgetCalculator/ViewController.swift
@@ -9,12 +9,217 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    var displayedNumber = 0.0
+    var numbersToCompute = [Double]()
+    var operations = [String]()
+    var operationClicked: Bool = false
+    var lastComputedNumber = 0.0
+    var lastOperationClicked = ""
+    var globalTotal = 0.0
+    
+    //for consecutive operation clicked beside equals
+    var plusOrMinus = 0
+    var multiplyOrDivide = 0
+    
+    var plusIsLastClicked: Bool = false
+    var minusIsLastClicked: Bool = false
+    var multiplyIsLastClicked: Bool = false
+    var divideIsLastClicked: Bool = false
+    var equalIsLastClicked: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        displayLabel.text = "0"
     }
-
-
+    
+    @IBAction func displayNumber(_ sender: UIButton) {
+        if displayLabel.text == "0" {
+            displayLabel.text = ""
+        }
+        
+        if !operationClicked {
+            displayLabel.text = displayLabel.text! + String(sender.tag)
+        }
+        else {
+            displayLabel.text = String(sender.tag)
+        }
+        
+        operationClicked = false
+    }
+    
+    @IBAction func compute(_ sender: UIButton)  {
+        var displayedOperation = "";
+        displayedNumber = Double(displayLabel.text!)!
+        
+        numbersToCompute.append(displayedNumber)
+        
+        displayedOperation = convertTagToOperation(sender.tag)
+        operations.append(displayedOperation)
+        
+        operationClicked = true
+        
+        if consecutiveOperationClicked(displayedOperation) == "plusOrMinus" {
+            var result = 0.0
+            
+            for i in 0..<(operations.count-1) {
+                if operations[i] == "+" {
+                    result += numbersToCompute[i] + numbersToCompute[i+1]
+                } else if operations[i] == "-" {
+                    result += numbersToCompute[i] - numbersToCompute[i+1]
+                }
+            }
+            
+        } else if consecutiveOperationClicked(displayedOperation) == "multiplyOrDivide" {
+            submit()
+        }
+    }
+    
+    @IBAction func equals()  {
+        if equalIsLastClicked == true {
+            if lastOperationClicked == "+" {
+                globalTotal = globalTotal + lastComputedNumber
+                displayLabel.text = String(removeTrailingZero(temp: globalTotal))
+            } else if lastOperationClicked == "-" {
+                globalTotal = globalTotal - lastComputedNumber
+                displayLabel.text = String(removeTrailingZero(temp: globalTotal))
+            } else if lastOperationClicked == "/" {
+                globalTotal = globalTotal / lastComputedNumber
+                displayLabel.text = String(removeTrailingZero(temp: globalTotal))
+            } else if lastOperationClicked == "*" {
+                globalTotal = globalTotal * lastComputedNumber
+                displayLabel.text = String(removeTrailingZero(temp: globalTotal))
+            }
+ 
+            resetArrays()
+            operationClicked = true
+            resetOperationsLastClicked()
+            equalIsLastClicked = true
+        }
+        else {
+            submit()
+            resetArrays()
+            operationClicked = true
+            resetOperationsLastClicked()
+            equalIsLastClicked = true
+        }
+    }
+    
+    // Light gray button
+    @IBAction func resetDisplay() {
+        displayLabel.text = "0"
+        globalTotal = 0.0
+        lastComputedNumber = 0.0
+        lastOperationClicked = ""
+        resetArrays()
+        resetOperationsLastClicked()
+    }
+    
+    @IBAction func addMinusSign() {
+        displayLabel.text = "-" + displayLabel.text!
+    }
+    
+    @IBAction func percentCalc() {
+        displayedNumber = Double(displayLabel.text!)!/100
+        displayLabel.text = String(displayedNumber)
+    }
+    
+    // convert operation tag to operation
+    func convertTagToOperation(_ tag: Int) -> String {
+        
+        var operation = ""
+        
+        if tag == 10 {
+            operation = "/"
+            divideIsLastClicked = true
+        } else if tag == 11 {
+            operation = "*"
+            multiplyIsLastClicked = true
+        } else if tag == 12 {
+            operation = "-"
+            minusIsLastClicked = true
+        } else if tag == 13 {
+            operation = "+"
+            plusIsLastClicked = true
+        }
+        
+        return operation
+    }
+    
+    //reset array when submit (equal) is clicked
+    func resetArrays() {
+        operations.removeAll()
+        numbersToCompute.removeAll()
+    }
+    
+    func resetOperationsLastClicked() {
+        plusIsLastClicked = false
+        minusIsLastClicked = false
+        multiplyIsLastClicked = false
+        divideIsLastClicked = false
+        equalIsLastClicked = false
+    }
+    
+    //remove trailing zero for double
+    func removeTrailingZero(temp: Double) -> String {
+        let tempVar = String(format: "%g", temp)
+        return tempVar
+    }
+    
+    func consecutiveOperationClicked(_ operation: String) -> String {
+        var operationToReturn: String = ""
+        
+        if operation == "+" || operation == "-" {
+            plusOrMinus = plusOrMinus + 1
+        } else if operation == "*" || operation == "/" {
+            multiplyOrDivide = multiplyOrDivide + 1
+        }
+        
+        if plusOrMinus == 2 {
+            plusOrMinus = 0
+            multiplyOrDivide = 0
+            operationToReturn = "plusOrMinus"
+        }
+        
+        if multiplyOrDivide == 2 {
+            plusOrMinus = 0
+            multiplyOrDivide = 0
+            operationToReturn =  "multiplyOrDivide"
+        }
+        
+        return operationToReturn
+        
+    }
+    
+    func submit() {
+        var result = 0.0
+        
+        displayedNumber = Double(displayLabel.text!)!
+        numbersToCompute.append(displayedNumber)
+        
+        for i in 0..<operations.count {
+            if operations[i] == "+" {
+                result += numbersToCompute[i] + numbersToCompute[i+1]
+            } else if operations[i] == "-" {
+                result += numbersToCompute[i] - numbersToCompute[i+1]
+            } else if operations[i] == "/" {
+                result += numbersToCompute[i] / numbersToCompute[i+1]
+            } else if operations[i] == "*" {
+                result += numbersToCompute[i] * numbersToCompute[i+1]
+            }
+        }
+        
+        displayLabel.text = removeTrailingZero(temp: result)
+        
+        globalTotal = result
+        lastOperationClicked = operations[operations.count - 1]
+        lastComputedNumber = numbersToCompute[numbersToCompute.count - 1]
+        
+        resetOperationsLastClicked()
+    }
+    
 }
 
